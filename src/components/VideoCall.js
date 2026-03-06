82201715b14a5e29a13511d9263d9f895dbbe795
wavemeetweb <abhilash123happy@gmail.com>
2026-03-06 18:41:43 +0530

    Update VideoCall.js

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -1,1 +1,23 @@
 
+import React, { useRef, useEffect } from "react";
+
+function VideoCall({ user }) {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    async function initCamera() {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      videoRef.current.srcObject = stream;
+    }
+    initCamera();
+  }, []);
+
+  return (
+    <div className="video-call">
+      <h2>Video Call</h2>
+      <video ref={videoRef} autoPlay playsInline />
+    </div>
+  );
+}
+
+export default VideoCall;
