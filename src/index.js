72dfb2ef47054ae1fdb10b91c0a3427cf3ce2ed1
wavemeetweb <abhilash123happy@gmail.com>
2026-03-07 10:31:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css"; // global styles
 
+// Create root and render App
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
