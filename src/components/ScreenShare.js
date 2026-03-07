a769e41422f5388a668d961e2abc71eb7d0f081d
wavemeetweb <abhilash123happy@gmail.com>
2026-03-07 09:47:03 +0530

    Update ScreenShare.js

--- src/components/ScreenShare.js
+++ src/components/ScreenShare.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function ScreenShare() {
+  return (
+    <div className="screen-share">
+      <h2>Screen Share</h2>
+      <p>Screen sharing feature coming soon!</p>
+    </div>
+  );
+}
+
+export default ScreenShare;
