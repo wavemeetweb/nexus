7c98669a3d3f549da2bbfad73392c1abc5c56e29
wavemeetweb <abhilash123happy@gmail.com>
2026-03-08 08:52:35 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "meetup-app.firebaseapp.com",
+  projectId: "meetup-app",
+  storageBucket: "meetup-app.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
